4f4397ab34e91a2b2daf4a41966977fcc27faa6a
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-13 18:04:26 +0500

    ✨ Add: Template with dynamic data

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # path('', views.ProductView.as_view()),
     path('', views.index),
     path('new/', views.new)
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView, View
+from django.views.generic.list import ListView
+from django.template.response import TemplateResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.shortcuts import render
 from .models import Product
 
@@ -9,6 +14,17 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
+
+
+# class ProductView(ListView):
+#     template_name = 'index.html'
+#     model = Product
+#
+#     # def get_context_data(self):
+#     #     products = Product.objects.all()
+#     #     return {'products': products}
+#     #
+#     # def post(self):
 
 
 def new(request):
