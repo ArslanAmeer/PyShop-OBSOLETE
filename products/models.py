52a9243180ea05656ff9136d89738abefbae0809
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-13 16:03:58 +0500

    ✨ Add: New offer model and migration

--- products/models.py
+++ products/models.py
@@ -6,3 +6,9 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code = models.CharField(max_length=20)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
