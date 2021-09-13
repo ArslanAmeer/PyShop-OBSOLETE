a71a39747caeaff6385be848d2d8bd7d0c46d4b5
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-13 15:27:12 +0500

    ✨ Add: Views and url

--- products/views.py
+++ products/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 
 # our first view function
+# /products -> index
 
 def index(request):
     return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('Hello to new World')
