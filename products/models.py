7b93dc453fd52dea9a40bdda0d683c4d59205a2f
Arslan Ameer <arslan.ameer@hotmail.com>
2021-09-13 15:40:14 +0500

    ✨ Add: product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
